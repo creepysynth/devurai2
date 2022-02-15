58376476e16a17e6b6929f012da2f10b7a6196ca
Roman Kostiuk <creepysynth@gmail.com>
2022-02-15 19:57:08 +0200

    Main file output correction.

--- main.php
+++ main.php
@@ -1,12 +1,12 @@
 <?php
-require 'periods.php';
+require 'Periods.php';
 
 $dates1 = file_get_contents('dates1.json');
 $dates2 = file_get_contents('dates2.json');
 $dates3 = file_get_contents('dates3.json');
 $dates4 = file_get_contents('dates4.json');
 
-echo periods($dates1);
-echo periods($dates2);
-echo periods($dates3);
-echo periods($dates4);
+echo (new Periods($dates1))->humanReadable();
+echo (new Periods($dates2))->humanReadable();
+echo (new Periods($dates3))->humanReadable();
+echo (new Periods($dates4))->humanReadable();
